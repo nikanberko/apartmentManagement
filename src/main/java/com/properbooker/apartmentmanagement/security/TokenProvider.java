1363e81b4903249516c609af27e6942bbe3f3b17
Nikola Pavkovic <nikola.pavkovic@infobip.com>
2023-08-29 20:24:56 +0200

    Removed unused code

--- src/main/java/com/properbooker/apartmentmanagement/security/TokenProvider.java
+++ src/main/java/com/properbooker/apartmentmanagement/security/TokenProvider.java
@@ -3,12 +3,8 @@ package com.properbooker.apartmentmanagement.security;
 import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jwts;
 import org.springframework.stereotype.Component;
-
 import java.io.Serializable;
-import java.util.Date;
 import java.util.function.Function;
-
-import static com.properbooker.apartmentmanagement.constants.securityConstants.Constants.SIGNING_KEY;
 
 @Component
 public class TokenProvider implements Serializable {
@@ -18,32 +14,6 @@ public class TokenProvider implements Serializable {
         return getClaimFromToken(token, Claims::getSubject);
     }
 
-   /* public String getUsernameFromToken(final String token) {
-        Claims claims = getAllClaimsFromToken(token);
-        Object userIdObject = claims.get("username");
-
-        if (userIdObject instanceof String) {
-            return (String) userIdObject;
-        } else if (userIdObject instanceof Integer) {
-            return String.valueOf(userIdObject);
-        } else {
-            throw new IllegalArgumentException("Invalid user ID type in token claims");
-        }    }*/
-    public String getUserIdFromToken(final String token) {
-        Claims claims = getAllClaimsFromToken(token);
-        Object userIdObject = claims.get("userId");
-
-        if (userIdObject instanceof String) {
-            return (String) userIdObject;
-        } else if (userIdObject instanceof Integer) {
-            return String.valueOf(userIdObject);
-        } else {
-            throw new IllegalArgumentException("Invalid user ID type in token claims");
-        }    }
-
-    public Date getExpirationDateFromToken(final String token) {
-        return getClaimFromToken(token, Claims::getExpiration);
-    }
 
     public <T> T getClaimFromToken(final String token, final Function<Claims, T> claimsResolver) {
         final Claims claims = getAllClaimsFromToken(token);
@@ -56,10 +26,5 @@ public class TokenProvider implements Serializable {
                 .setSigningKey("secret-key".getBytes())
                 .parseClaimsJws(token)
                 .getBody();
-    }
-
-    private Boolean isTokenExpired(final String token) {
-        final Date expiration = getExpirationDateFromToken(token);
-        return expiration.before(new Date());
     }
 }
